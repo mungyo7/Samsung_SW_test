e7b86f286bdd5b22604d2378aec1c558c5ea55fd
mungyo7 <mungyo7@yonsei.ac.kr>
2025-04-12 15:38:12 +0900

    .

---
+++ 문제 연습/test.py
@@ -0,0 +1,6 @@
+import sys
+sys.stdin=open("input.txt","r")
+
+a,b = map(int,input().split())
+
+print(a,b)
--- 문제 연습/메두사.py
+++ 문제 연습/메두사.py
@@ -7,9 +7,31 @@
 0 0 1 1
 1 0 0 0
 
+6 4
+3 1 1 2
+3 5 1 4 0 4 1 3
+0 0 0 0 1 0
+0 1 0 0 1 1
+1 1 0 0 0 0
+0 0 1 0 1 1
+0 0 0 0 0 0
+0 0 0 0 1 1
 
-
+9 3
+3 1 1 2
+4 2 4 4 6 6
+0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0
 """
+import sys
+sys.stdin=open("input.txt","r")
 
 N, M = map(int, input().split()) # N : 마을의 크기, M : 전사의 수
 smi, smj, emi, emj = map(int, input().split()) # 메두사 시작, 끝 좌표
@@ -82,27 +104,32 @@ for i in marr:
 #     print(i)
 
 flag = 0 # 기본은 0, 전사 만나면 1로 바뀜
-
+# m_route_list=[(0,4)]
 for mi, mj in m_route_list: # (mi,mj) 현재 메두사 좌표
-
+    max_cnt = 0
+    max_list=[]
     for idx in (0,4,6,2): # 상 하 좌 우
+        cnt=0
         di,dj = dr_i[idx], dr_j[idx]
         nmi, nmj = mi+di, mj+dj
         mx_stone = 0
         sight_arr = [[0] * N for _ in range(N)]
         ti, tj = 0,0
+        nmarr=[x[:] for x in marr]
         while (0<=nmi<N and 0<=nmj<N):
-            if marr[nmi][nmj] == 1 and flag==0: # 전사 만나면 flag 1로 바꿈
+            if marr[nmi][nmj] > 0 and flag==0: # 전사 만나면 flag 1로 바꿈
                 flag = 1
                 mx_stone += 1
+
 
             if flag == 0: # 아직 전사 안만남
                 sight_arr[nmi][nmj] = 1
                 nmi = nmi+di
                 nmj = nmj+dj
             else: # 전사 뒤쪽
-                if marr[nmi][nmj] == 1:
+                if marr[nmi][nmj] > 0:
                     sight_arr[nmi][nmj] = 1
+                    nmarr[nmi][nmj] -= 1
                 else:
                     sight_arr[nmi][nmj] = 2
                 nmi = nmi + di
@@ -110,37 +137,61 @@ for mi, mj in m_route_list: # (mi,mj) 현재 메두사 좌표
             # print('d')
         flag = 0
 
-        print(f'dr : {di},{dj}, mx_stone : {mx_stone}')
-        print("\n[sight_arr]")
-        for i in sight_arr:
-            print(i)
+        # print(f'\n(mi,mj) : ({mi},{mj}) dr : {di},{dj}, mx_stone : {mx_stone}')
+        # print("[sight_arr]")
+        # for i in sight_arr:
+        #     print(i)
 
         for idx2 in ((idx+1)%8, (idx-1)%8): # 상 하 좌 우
             di2, dj2 = dr_i[idx2], dr_j[idx2] # 대각선 이동 방향
-            nmi, nmj = mi + di2, mj + dj2 # 메두사 대각선 좌표
-            tlist=[]
-            while (0 <= nmi < N and 0 <= nmj < N):
-                if marr[nmi][nmj] == 1 and flag == 0:  # 전사 만나면 flag 1로 바꿈
-                    flag = 1
-                    mx_stone += 1
-                    ti, tj = nmi, nmj  # 대각선 원점
-                    tlist.append((ti,tj))
-                    while (0 <= ti < N and 0 <= tj < N):
-                        ti, tj = ti + di2, tj + dj2  # 메두사 대각선 좌표
-                        tlist.append((ti,tj))
-                if (nmi, nmj) in tlist: # 대각선 라인에 있으면 flag 1로 바꿈
-                    flag = 1
+            tlist = [] # 시야의 전사 원점 좌표
+            for mul in range(1,N+1):
+                nmi, nmj = mi + di2*mul, mj + dj2*mul # 메두사 대각선 좌표
+                flag2 = 0
+                flag = 0
+                while (0 <= nmi < N and 0 <= nmj < N):
+                    if marr[nmi][nmj] > 0 and flag == 0:  # 전사 만나면 flag 1로 바꿈
+                        flag = 1
+                        mx_stone += 1
+                        ti, tj = nmi, nmj  # 대각선 원점
 
-                if flag == 0:  # 아직 전사 안만남
-                    sight_arr[nmi][nmj] = 1
-                    nmi = nmi + di
-                    nmj = nmj + dj
-                else:  # 전사 뒤쪽
-                    if marr[nmi][nmj] == 1:
+                        while (0 <= ti < N and 0 <= tj < N):
+                            tlist.append((ti,tj))
+                            ti, tj = ti + di2, tj + dj2  # 메두사 대각선 좌표
+
+                    if (nmi, nmj) in tlist: # 대각선 라인에 있으면 flag 1로 바꿈
+                        flag = 1
+
+                    if flag == 0:  # 아직 전사 안만남 아무것도 없으면 기존 방향 일자 탐색
                         sight_arr[nmi][nmj] = 1
+                        nmi = nmi + di
+                        nmj = nmj + dj
+                    elif flag == 1 and flag2==0:  # 시선 라인의 첫 전사
+                        if marr[nmi][nmj] > 0: # 전사 뒤쪽은 전사 있어도 2표시
+                            sight_arr[nmi][nmj] = 1
+                            nmarr[nmi][nmj]-=1
+                            flag2=1 #flag2가 1이면 무조건 2
+                        else:
+                            sight_arr[nmi][nmj] = 2
+                        nmi = nmi + di
+                        nmj = nmj + dj
                     else:
                         sight_arr[nmi][nmj] = 2
-                    nmi = nmi + di
-                    nmj = nmj + dj
-                # print('d')
+                        nmi = nmi + di
+                        nmj = nmj + dj
+                    # print('d')
             flag = 0
+        if mx_stone>max_cnt:
+            max_cnt=mx_stone
+            max_list.append((di,dj,mx_stone))
+
+
+        print(f'\n(mi,mj) : ({mi},{mj}) dr : {di},{dj}, mx_stone : {mx_stone}, mx_cnt : {max_cnt}')
+        print(tlist)
+        print("[sight_arr]")
+        for i in sight_arr:
+            print(i)
+        print("[nmarr]")
+        for i in nmarr:
+            print(i)
+    print(f'max_cnt : {max_cnt}')
